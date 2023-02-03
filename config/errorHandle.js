08151a52a4638ef2e9f0734b0207f028ed47c2f9
ptdr1516 <71786648+ptdr1516@users.noreply.github.com>
2023-02-03 14:13:07 +0530

    error handling

---
+++ config/errorHandle.js
@@ -0,0 +1,12 @@
+module.exports.handleError = function (err, req, res, next) {
+    if (err) {
+        req.code = 401;
+        req.info =
+            "Authorization failed! Invalid Authorization key or Password or Username!!";
+        return res.status(req.code).json({
+            message: req.info,
+        });
+    } else {
+        next();
+    }
+};
