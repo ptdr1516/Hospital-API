3d7039233dcb7bdc3e5c300578f6c78da17641b9
ptdr1516 <71786648+ptdr1516@users.noreply.github.com>
2023-02-03 14:14:30 +0530

    auth handling using passport

---
+++ config/passport-local-strategy.js
@@ -0,0 +1,34 @@
+const passport = require("passport");
+const localStrategy = require("passport-local").Strategy;
+const Doctor = require("../models/doctor");
+
+// authentication with local strategy
+passport.use(
+    new localStrategy(
+        {
+            usernameField: "email",
+            passReqToCallback: true,
+        },
+        function (req, email, password, done) {
+            // find a doctor and establish identity
+            Doctor.findOne({ email: email }, function (err, doctor) {
+                if (err) {
+                    console.log("error finding in user --> passport");
+                    req.info = "Something's not Right";
+                    req.code = 500;
+                    return done(err);
+                }
+                if (!doctor || doctor.password != password) {
+                    console.log("Invalid username password");
+                    req.code = 401;
+                    req.info = "Invalid username password";
+                    return done(null, false);
+                } else {
+                    console.log(doctor);
+                    req.code = 200;
+                    return done(null, doctor);
+                }
+            });
+        }
+    )
+);
