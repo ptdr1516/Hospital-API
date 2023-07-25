5e46caae479f30f88865b004336214f38b5591bf
ptdr1516 <71786648+ptdr1516@users.noreply.github.com>
2023-07-25 15:47:07 +0530

    first commit

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost/Hospital-API");
+mongoose.connect("mongodb+srv://ptdr1516:ptdr105283921@cluster0.k7srh0y.mongodb.net/hospitalApi");
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "Error In Connecting Database"));
 db.once("open", function () {
